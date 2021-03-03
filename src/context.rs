2bb2b63ae4e1eff76e2b5019f091a37d588fc44f
Eric Rochester <eric.rochester@willowtreeapps.com>
2021-03-03 06:32:36 -0500

    context

--- src/context.rs
+++ src/context.rs
@@ -1,7 +1,16 @@
+use dataloader::BatchFn;
+use dataloader::BatchFuture;
+use diesel::dsl::*;
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use juniper;
 use r2d2;
 use r2d2_diesel::ConnectionManager;
+use futures::Future;
+use futures::future::ok;
+
+use crate::models::Node;
+use crate::schema::nodes;
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
@@ -17,3 +26,18 @@ pub struct Context {
 }
 
 impl juniper::Context for Context {}
+
+impl BatchFn<i32, Node> for Context {
+    type Error = String;
+
+    fn load(&self, keys: &[i32]) -> BatchFuture<Node, Self::Error> {
+        self.pool.get()
+            .map_err(|err| format!("Error getting connection pool: {}", &err))
+            .and_then(|connection|
+                nodes::table
+                    .filter(nodes::dsl::id.eq(any(keys)))
+                    .load::<Node>(&*connection)
+                    .map_err(|err| format!("Unable to load nodes: {}", &err)))
+            .boxed()
+    }
+}
